68cf32031f5c1af9fb5a81612c0b2de8bc2aaeea
Muhammad bin masood <muhammad.masood@unitedsol.net>
2025-10-02 17:36:26 +0500

    Custom Package

---
+++ packages/wire-content/src/WireContentServiceProvider.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace WireContent;
+
+use Spatie\LaravelPackageTools\Package;
+use Spatie\LaravelPackageTools\PackageServiceProvider;
+use WireContent\Commands\InstallCommand;
+use WireContent\Commands\SkeletonCommand;
+
+class WireContentServiceProvider extends PackageServiceProvider
+{
+    public function configurePackage(Package $package): void
+    {
+        /*
+         * This class is a Package Service Provider
+         *
+         * More info: https://github.com/spatie/laravel-package-tools
+         */
+        $package
+            ->name('wire-content')
+            ->hasConfigFile()
+            ->hasViews()
+            ->hasMigration('create_migration_table_name_table')
+            ->hasCommand(InstallCommand::class);
+    }
+}
